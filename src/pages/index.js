7c9053ae9a688d9c48c4070ed7f43b8bf11abbd5
Jamilly Souza <jamilly.gcsouza@gmail.com>
2020-06-24 19:56:04 -0300

    customers
    customer table

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,16 @@ import {Route, Switch} from 'react-router-dom';
 
 import Home from './Home';
 import Movies from './Movies';
+import Customers from './Customers';
 
 const Pages = () => {
   return (
     <Switch>
       <Route path="/" exact>
         <Home />
+      </Route>
+      <Route path="/customers" exact>
+        <Customers />
       </Route>
       <Route path="/movies" exact>
         <Movies />
